a2e288af814cbc9b8e96db62ce8ac73f7a107d3d
Alexandre Ciorascu <alexandre.ciorascu@u-psud.fr>
2022-03-28 23:05:46 +0200

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import search from "./assets/icons/search.svg";
+import PokemonCart from "./components/pokemonCart/PokemonCart";
 
 import Pokedex from "pokedex-promise-v2"; //with pokedex-promise-v2
 const P = new Pokedex();
 
 const App = () => {
-  const [data, setData] = useState([{}]);
+  const [pokemon, setPokemon] = useState({});
   const [searchTerm, setSearchTerm] = useState("Pikachu");
 
-  const searchPokemon = async (name) => {};
-
-  useEffect(() => {
-    P.getPokemonByName("1") // with Promise
+  const searchPokemon = async (name) => {
+    P.getPokemonByName(name.toLowerCase()) // with Promise
       .then((response) => {
-        setData(response);
+        setPokemon(response);
         console.log(response);
       })
       .catch((error) => {
         console.log("There was an ERROR: ", error);
+        setPokemon(null);
       });
-  }, []);
+  };
+
   return (
     <div className="app padding">
       <h1>PokéWeb</h1>
@@ -36,6 +37,16 @@ const App = () => {
           onClick={() => searchPokemon(searchTerm)}
         />
       </div>
+
+      {pokemon != null ? (
+        <div className="container">
+          <PokemonCart pokemon={pokemon} />
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Pokemon :/</h2>
+        </div>
+      )}
     </div>
   );
 };
--- client/src/components/pokemonCart/PokemonCart.jsx
+++ client/src/components/pokemonCart/PokemonCart.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import "pokemonCart.css"
+import "./pokemonCart.css"
 
-const PokemonCart = () => {
-  return (
-    <div>PokemonCart</div>
-  )
+const PokemonCart = (pokemon) => {
+    return (
+    <div className="pokemon__cart">
+        <div>
+            <p>{pokemon.name}</p>
+        </div>
+
+        <div>
+            <img src={pokemon.pokemon.sprites.front_default} alt={pokemon.pokemon.name}/>
+        </div>
+        <div>
+            <span>{pokemon.Type}</span>
+            <h3>{pokemon.Title}</h3>
+        </div>
+    </div>);
 }
 
 export default PokemonCart
